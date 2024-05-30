40667842262c89f2c5a7bf7eee4655580a17b18f
fiftinmen <fiftin.men@gmail.com>
2024-05-30 19:38:33 +0300

    mission accomplished; send to test

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE urls (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP
+);
+CREATE TABLE url_checks (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id INT REFERENCES urls (id),
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description VARCHAR(255),
+    created_at TIMESTAMP
+);
