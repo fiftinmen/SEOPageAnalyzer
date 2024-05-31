0db2bdaa2a98ab3e09259a0ed3519c4628b1ceb9
fiftinmen <fiftin.men@gmail.com>
2024-05-31 12:17:19 +0300

    refactor; remove reusing build from makefile;

--- Makefile
+++ Makefile
@@ -1,7 +1,3 @@
-setup: install just-build package-force-reinstall
-
-setup-linux: install just-build package-force-reinstall-linux
-
 dev:
 	poetry run flask --app page_analyzer:app run
 
@@ -15,31 +11,13 @@ PORT ?= 8000
 start:
 	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
 
-install:
-	poetry install
-
-just-build:
-	poetry build
-
 lint:
 	poetry run flake8 page_analyzer
 
 selfcheck:
 	poetry check
 
-check: selfcheck test lint
-
-build: check
-	poetry build
-
-package-install:
-	python -m pip install --user dist/*.whl
+check: selfcheck lint
 
 remove-envs:
 	rm -rf .venv && poetry env remove --all
-
-package-force-reinstall:
-	python -m pip install --user --force-reinstall dist/*.whl
-
-package-force-reinstall-linux:
-	python3 -m pip install --user --force-reinstall dist/*.whl
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -5,19 +5,20 @@ from flask import (
     flash,
     get_flashed_messages,
     redirect,
+    g,
 )
+import validators
 from bs4 import BeautifulSoup
 from requests import (
     get,
     HTTPError
 )
 import psycopg2
-from psycopg2 import (
-    pool
-)
 import os
 from dotenv import load_dotenv
-from re import fullmatch
+from urllib.parse import urlparse
+
+app = Flask(__name__)
 
 load_dotenv()
 
@@ -25,19 +26,20 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 if not DATABASE_URL:
     raise ValueError("No DATABASE_URL set for Flask application")
-print(1)
-try:
-    connection_pool = pool.SimpleConnectionPool(
-        1,  # Минимальное количество соединений в пуле
-        10,  # Максимальное количество соединений в пуле
-        DATABASE_URL
-    )
-except (Exception, psycopg2.DatabaseError) as error:
-    print(f"Error while connecting to PostgreSQL: {error}")
-    raise
-print(2)
-app = Flask(__name__)
-print(3)
+
+
+def get_cursor():
+    if 'db' not in g:
+        try:
+            app.config['postgreSQL_pool'] = psycopg2.connect(DATABASE_URL)
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(f"Error while connecting to PostgreSQL: {error}")
+            raise
+        g.db = app.config['postgreSQL_pool']
+    return g.db.cursor()
+
+
 app.secret_key = 'super secret key'
 MESSAGES = {
     "success": "Страница успешно добавлена",
@@ -46,26 +48,14 @@ MESSAGES = {
 }
 
 
-def is_valid_url(url):
-    if not isinstance(url, str):
-        return False
-    if len(url) > 255:
-        return False
-    return fullmatch(
-        'http[s]?:[/]{2}([A-za-z.0-9_])+[A-za-z/?=+%&0-9-]*', url
-    ) is not None
-
-
 def get_url_data_by_field(field, value):
     # Получение соединения из пула
-    with connection_pool.getconn() as conn:
-        with conn.cursor() as cursor:
-            cursor = conn.cursor()
-            cursor.execute(f"""
-            SELECT * FROM urls
-            WHERE {field} = %s
-            """, (value,))
-            return cursor.fetchone() or ['']
+    cursor = get_cursor()
+    cursor.execute(f"""
+    SELECT * FROM urls
+    WHERE {field} = %s
+    """, (value,))
+    return cursor.fetchone() or ['']
 
 
 def parse_page(page):
@@ -82,14 +72,16 @@ def parse_page(page):
 
 
 def handle_url(url):
-    if not is_valid_url(url):
-        return MESSAGES['danger'], "danger"
+    url_parts = urlparse(url)
+    url = f"{url_parts.scheme}://{url_parts.netloc}"
+    if not validators.url(url):
+        return url, MESSAGES['danger'], "danger"
     try:
         if url not in get_url_data_by_field('name', url):
-            return MESSAGES['success'], "success"
+            return url, MESSAGES['success'], "success"
     except TypeError as e:
         return ' '.join([url, *e.args]), "danger"
-    return MESSAGES['warning'], "warning"
+    return url, MESSAGES['warning'], "warning"
 
 
 @app.route('/')
@@ -104,16 +96,15 @@ def index():
 @app.post('/urls')
 def add_url():
     url = request.form.to_dict().get('url')
-    message, result = handle_url(url)
+    url, message, result = handle_url(url)
     flash(message, result)
     if result == 'success':
-        with connection_pool.getconn() as conn:
-            with conn.cursor() as cursor:
-                cursor = conn.cursor()
-                cursor.execute("""
-                INSERT INTO urls (name, created_at)
-                VALUES (%s, NOW())
-                """, (url,))
+        cursor = get_cursor()
+        cursor.execute("""
+        INSERT INTO urls (name, created_at)
+        VALUES (%s, NOW())
+        """, (url,))
+        g.db.commit()
     if result in {'success', 'warning'}:
         url_id = get_url_data_by_field('name', url)[0]
         return redirect(f'urls/{url_id}', 302)
@@ -121,67 +112,63 @@ def add_url():
 
 
 def get_checks_data(url_id):
-    with connection_pool.getconn() as conn:
-        with conn.cursor() as cursor:
-            cursor = conn.cursor()
-            cursor.execute(
-                """
-                SELECT
-                    id,
-                    status_code,
-                    h1,
-                    title,
-                    description,
-                    created_at FROM url_checks
-                WHERE url_id = %s
-                """,
-                (int(url_id),)
-            )
-            if cursor:
-                return cursor.fetchall()
+    cursor = get_cursor()
+    cursor.execute(
+        """
+        SELECT
+            id,
+            status_code,
+            h1,
+            title,
+            description,
+            created_at FROM url_checks
+        WHERE url_id = %s
+        """,
+        (int(url_id),)
+    )
+    if cursor:
+        return cursor.fetchall()
 
 
 @app.get('/urls')
 def show_urls_list():
     messages = get_flashed_messages(with_categories=True)
     urls = None
-    with connection_pool.getconn() as conn:
-        with conn.cursor() as cursor:
-            cursor = conn.cursor()
-            cursor.execute(
-                """
-                WITH url_last_checks(url_id, status_code, created_at) AS (
-                    SELECT
-                        url_checks.url_id as url_id,
-                        url_checks.status_code as status_code,
-                        url_checks.created_at as created_at
-                    FROM url_checks
-                    LEFT JOIN urls
-                        ON url_checks.url_id = urls.id
-                    ORDER BY url_checks.created_at DESC
-                    LIMIT 1
-                )
-                SELECT
-                    urls.id as id,
-                    urls.name as name,
-                    url_last_checks.status_code as status_code,
-                    url_last_checks.created_at as created_at
-                    FROM urls
-                LEFT JOIN url_last_checks
-                ON urls.id = url_last_checks.url_id
-                """
-            )
-            if cursor:
-                urls = [
-                    {
-                        'id': rec[0],
-                        'name': rec[1],
-                        'status_code': rec[2] or '',
-                        'last_check_date': rec[3].date() if rec[3]
-                        is not None else ''
-                    }
-                    for rec in cursor.fetchall()
-                ]
+    cursor = get_cursor()
+    cursor.execute(
+        """
+        WITH url_last_checks(url_id, status_code, created_at) AS (
+            SELECT
+                url_checks.url_id as url_id,
+                url_checks.status_code as status_code,
+                url_checks.created_at as created_at
+            FROM url_checks
+            LEFT JOIN urls
+                ON url_checks.url_id = urls.id
+            ORDER BY url_checks.created_at DESC
+            LIMIT 1
+        )
+        SELECT
+            urls.id as id,
+            urls.name as name,
+            url_last_checks.status_code as status_code,
+            url_last_checks.created_at as created_at
+            FROM urls
+        LEFT JOIN url_last_checks
+        ON urls.id = url_last_checks.url_id
+        """
+    )
+    if cursor:
+        urls = [
+            {
+                'id': rec[0],
+                'name': rec[1],
+                'status_code': rec[2] or '',
+                'last_check_date': rec[3].date() if rec[3]
+                is not None else ''
+            }
+            for rec in cursor.fetchall()
+        ]
     return render_template(
         'urls.html',
         messages=messages,
@@ -211,24 +198,30 @@ def check_url(url_id):
         response = get(url)
         response.raise_for_status()
         h1, title, description = parse_page(response.text)
-        with connection_pool.getconn() as conn:
-            with conn.cursor() as cursor:
-                cursor = conn.cursor()
-                cursor.execute("""
-                INSERT INTO url_checks (
-                    url_id,
-                    status_code,
-                    h1,
-                    title,
-                    description,
-                    created_at)
-                VALUES (%s, %s, %s, %s, %s, NOW())
-                """, (
-                    url_id,
-                    response.status_code,
-                    h1,
-                    title,
-                    description))
+        cursor = get_cursor()
+        cursor.execute("""
+        INSERT INTO url_checks (
+            url_id,
+            status_code,
+            h1,
+            title,
+            description,
+            created_at)
+        VALUES (%s, %s, %s, %s, %s, NOW())
+        """, (
+            url_id,
+            response.status_code,
+            h1,
+            title,
+            description))
+        g.db.commit()
     except HTTPError:
         flash('Произошла ошибка при проверке', 'danger')
     return redirect(f'/urls/{url_id}', 302)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    db = g.pop('db', None)
+    if db is not None:
+        app.config['postgreSQL_pool'].close()
